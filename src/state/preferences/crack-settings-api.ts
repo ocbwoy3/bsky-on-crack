27ca2c44ad66f44d117b482f2d399fc9e9a6c4a8
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2026-01-13 20:41:27 +0200

    chore: update appview verifiers

--- src/state/preferences/crack-settings-api.ts
+++ src/state/preferences/crack-settings-api.ts
@@ -41,15 +41,35 @@ export type CrackSettingsSection = {
   items: CrackSettingsItemWithPredicate[]
 }
 
+/**
+ * Bluesky AppView's trusted verifiers
+ * https://blu.ski/trusted
+ */
 export const APPVIEW_DEFAULT_VERIFIERS: string[] = [
-  'did:plc:z72i7hdynmk6r22z27h6tvur', // <-- Bluesky
-  'did:plc:inz4fkbbp7ms3ixufw6xuvdi', // <-- WIRED
-  'did:plc:b2kutgxqlltwc6lhs724cfwr', // <-- The Athletic
-  'did:plc:eclio37ymobqex2ncko63h4r', // <-- The New York Times
+  'did:plc:z72i7hdynmk6r22z27h6tvur', // Bluesky
+  'did:plc:eclio37ymobqex2ncko63h4r', // The New York Times
+  'did:plc:inz4fkbbp7ms3ixufw6xuvdi', // WIRED
+  'did:plc:b2kutgxqlltwc6lhs724cfwr', // The Athletic
+  'did:plc:wmho6q2uiyktkam3jsvrms3s', // NBC News
+  'did:plc:sqbswn3lalcc2dlh2k7zdpuw', // Yahoo Finance
+  'did:plc:y3xrmnwvkvsq4tqcsgwch4na', // The Globe and Mail
+  'did:plc:d2jith367s6ybc3ldsusgdae', // Los Angeles Times
+  'did:plc:dzezcmpb3fhcpns4n4xm4ur5', // CNN
+  'did:plc:5u54z2qgkq43dh2nzwzdbbhb', // Financial Times
+  'did:plc:xwqgusybtrpm67tcwqdfmzvy', // IGN
+  'did:plc:k5nskatzhyxersjilvtnz4lh', // The Washington Post
+  'did:plc:fivojrvylkim4nuo3pfqcf3k', // Microsoft
+
+  // not listed in blu.ski:
+  'did:plc:2w45zyhuklwihpdc7oj3mi63', // Forbes
 ]
 
-export const NEG_APPVIEW_VERIFIERS_LABELER_SUB: {[did: string]: string[]} = {
-  // anti-transphobia labeler
+/**
+ * Labeler verifier negation list
+ * (e.g. unverifying transphobic news outlets such as the new york times and washington post)
+ */
+export const LABELER_NEG_VERIFIERS: {[did: string]: string[]} = {
+  // asukafield.xyz
   'did:plc:4ugewi6aca52a62u62jccbl7': [
     'did:plc:eclio37ymobqex2ncko63h4r', // <-- nyt
   ],
